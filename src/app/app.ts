6c9bd7e8ef0873c1bfa0aef032d1de39c0bbdbc7
Vladimir Abraamyan <147451787+Kerop1981@users.noreply.github.com>
2025-06-15 18:17:34 +0300

    Revert "feat(channel): форма создания канала (локально, без Firestore)"

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CreateChannel } from './components/create-channel/create-channel';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CreateChannel],
-  template: `<app-create-channel></app-create-channel>`
-
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
 })
 export class App {
   protected title = 'clon-TG';
